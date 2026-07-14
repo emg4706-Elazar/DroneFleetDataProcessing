43e683d537e6dad2c15c31722357c92d180c1947
אלעזר גרשי <emg4706@gmail.com>
2026-07-14 10:51:36 +0300

    first commit init project

---
+++ DroneFleetDataProcessing/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DroneFleetDataProcessing.src
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
