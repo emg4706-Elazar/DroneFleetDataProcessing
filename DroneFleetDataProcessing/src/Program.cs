9ff66caa24da239b7cc29e100d8b6084239d918c
אלעזר גרשי <emg4706@gmail.com>
2026-07-15 16:32:25 +0300

    fix conflicts

--- DroneFleetDataProcessing/src/Program.cs
+++ DroneFleetDataProcessing/src/Program.cs
@@ -16,24 +16,32 @@ namespace DroneFleetDataProcessing.src
         public static void Main()
         {
             string filepathInput = GetInputPath("drones_raw.json");
-            string filenameOutput = "";
-            
+            string filenameOutput = "drones_clean.json";
+
             try
             {
                 DroneDataLoader loader = new DroneDataLoader();
-                List<Drone> drones = loader.Load(filepathInput);
+                List<Drone> allDrones = loader.Load(filepathInput);
+
+                List<Drone> validDrones = new List<Drone>();
+                List<Drone> rejectedDrones = new List<Drone>();
+
+                var validator = new DroneValidator();
+                var storer = new DronesDataValidator(validator);
+
+                storer.ValidateFleet(allDrones, validDrones, rejectedDrones);
 
 
                 DroneDataSaver saver = new DroneDataSaver();
-                saver.Save(drones, filenameOutput);
+                saver.Save(validDrones, filenameOutput);
                 Console.WriteLine("The Process completed successfuly");
-
             }
+
             catch (DroneDataLoaderException e)
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
-            catch(DroneDataSaverException e)
+            catch (DroneDataSaverException e)
             {
                 Console.WriteLine($"Error: {e.Message}");
             }
